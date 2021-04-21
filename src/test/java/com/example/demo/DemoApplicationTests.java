a10cfb34fc3662c517edd4fca318287957e0dcd7
pealipala <1029102110@qq.com>
2021-04-21 23:14:20 +0800

    第四种类型:header

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -12,15 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
 
-@Component
 @SpringBootTest
 class DemoApplicationTests {
 
     @Autowired
     RabbitTemplate rabbitTemplate;
-
-
-
 
     @Test
     void contextLoads1() {
